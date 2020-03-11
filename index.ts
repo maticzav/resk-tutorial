a736a9c3841730deb3e2595799695c87fc4a319a
maticzav <matic.zavadlal@gmail.com>
2020-03-11 21:49:59 +0100

    :sparkles: create tutorial

---
+++ index.ts
@@ -0,0 +1,9 @@
+/* resk start "example" */
+/* resk start "<action-name-start"> */
+const libraries = {
+  resk: {
+    cool: 9001,
+  },
+}
+/* resk start "<action-name-end"> */
+/* resk end "example" */
